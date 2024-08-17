7fae8d65c25119e135107af27f4f4ae1873db21c
takashimaso0629 <74915567+takashimaso0629@users.noreply.github.com>
2024-08-18 02:02:26 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ function PageTopAnime() {
 
     var scroll = $(window).scrollTop(); //スクロール値を取得
     if (scroll >= 200){//200pxスクロールしたら
-      $('#page-top').removeClass('DownMove');   // DownMoveというクラス名を除去して
-      $('#page-top').addClass('UpMove');      // UpMoveというクラス名を追加して出現
+      $('#page-top').removeClass('DownMove1');   // DownMoveというクラス名を除去して
+      $('#page-top').addClass('UpMove1');      // UpMoveというクラス名を追加して出現
     }else{//それ以外は
-      if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名が既に付与されていたら
-        $('#page-top').removeClass('UpMove'); //  UpMoveというクラス名を除去し
-        $('#page-top').addClass('DownMove');  // DownMoveというクラス名を追加して非表示
+      if($('#page-top').hasClass('UpMove1')){//UpMoveというクラス名が既に付与されていたら
+        $('#page-top').removeClass('UpMove1'); //  UpMoveというクラス名を除去し
+        $('#page-top').addClass('DownMove1');  // DownMoveというクラス名を追加して非表示
       }
     }
     
@@ -18,7 +18,7 @@ function PageTopAnime() {
       var pos = (scroll+wH) - footerPos+10 //スクロールの値＋画面の高さからfooterの位置＋10pxを引いた場所を取得し
       $('#page-top').css('bottom',pos); //#page-topに上記の値をCSSのbottomに直接指定してフッター手前で止まるようにする
     }else{//それ以外は
-      if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名がついていたら
+      if($('#page-top').hasClass('UpMove1')){//UpMoveというクラス名がついていたら
         $('#page-top').css('bottom','10px');// 下から10pxの位置にページリンクを指定
       }
     }
@@ -118,4 +118,52 @@ $(window).on('load',function(){
   });
   //=====ここまで背景が伸びた後に動かしたいJSをまとめる
       
+});
+
+
+
+var beforePos = 0;//スクロールの値の比較用の設定
+
+//スクロール途中でヘッダーが消え、上にスクロールすると復活する設定を関数にまとめる
+function ScrollAnime() {
+    var elemTop = $('#area-2').offset().top;//#area-2の位置まできたら
+  var scroll = $(window).scrollTop();
+    //ヘッダーの出し入れをする
+    if(scroll == beforePos) {
+    //IE11対策で処理を入れない
+    }else if(elemTop > scroll || 0 > scroll - beforePos){
+    //ヘッダーが上から出現する
+    $('#header').removeClass('UpMove'); //#headerにUpMoveというクラス名を除き
+    $('#header').addClass('DownMove');//#headerにDownMoveのクラス名を追加
+    }else {
+    //ヘッダーが上に消える
+        $('#header').removeClass('DownMove');//#headerにDownMoveというクラス名を除き
+    $('#header').addClass('UpMove');//#headerにUpMoveのクラス名を追加
+    }
+    
+    beforePos = scroll;//現在のスクロール値を比較用のbeforePosに格納
+}
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
+});
+
+
+
+
+//リンク先のidまでスムーススクロール
+//※ページ内リンクを行わない場合は不必要なので削除してください
+    var headerH = $("#header").outerHeight(true);//headerの高さを取得    
+    $('#g-navi li a').click(function () {
+  var elmHash = $(this).attr('href'); 
+  var pos = $(elmHash).offset().top-headerH;//header分の高さを引いた高さまでスクロール
+  $('body,html').animate({scrollTop: pos}, 1000);
+  return false;
 });
